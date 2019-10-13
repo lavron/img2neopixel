273a8c26717333de7b1ff40e79705c0a8058df4e
lavron <lavron@lavron.info>
2019-10-13 15:04:33 +0300

    cahnges

--- animation.py
+++ animation.py
@@ -2,7 +2,7 @@
 
 from random import randrange
 import neopixel
-from datetime import datetime
+import datetime
 from PIL import Image
 
 
@@ -12,13 +12,13 @@ def one_chance_from(value):
 
 
 class Animation:
-    def __init__(self, image, leds_num, brightness, speed, intencivity):
+    def __init__(self, image_src, leds_num, brightness, speed, intencivity):
 
         self.brightness = brightness
         self.speed = speed / 1000 # rows per ms
         self.intencivity = intencivity * 1000 # new flame per ms
         
-        self.image = Image(image)   
+        self.image = Image.open(image_src)   
         
         width, height = self.image.size
         proportion = height / width
@@ -29,7 +29,7 @@ class Animation:
         
         self.active_row = [(0,0,0)] * self.width
         
-        self.start_ms = datetime.now() 
+        self.start_ms = datetime.datetime.now() 
         self.now_ms =  self.start_ms
 
     
@@ -40,7 +40,7 @@ class Animation:
 
         width = randrange(min_width, self.image.width)
         heigh = randrange(min_heigh, self.image.height)
-        opacity = randrange(0.1, self.brightness, 0.1)
+        opacity = randrange(1, int(self.brightness * 10)) / 10
 
         new_image = Image(self.image).resize(width, heigh)
 
@@ -51,19 +51,20 @@ class Animation:
         del new_image
 
     def process(self):
-        elapsed = datetime.timedelta(self.start_ms)
+        elapsed = datetime.datetime.now()  - self.start_ms 
         
         move = self.height * ((elapsed.microseconds * 1000) % self.speed) 
+
         
-        for i in self.width:
-            self.active_row[i] =  self.image.getpixel(i, move)
+        for i in range(self.width):
+            self.active_row[i] =  self.image.getpixel((i, move))
             
         
         bg = Image.new('RGBA', (self.image.size), (0,0,0))
         
-        bg.paste(self.image, 0,0 )
+        bg.paste(self.image, (0,0) )
         
         if one_chance_from(self.intencivity):
             self.add_image()
             
-        del bg
+        del bg  
--- main.py
+++ main.py
@@ -4,27 +4,31 @@ from animation import Animation
 from random import randrange
 import time
 import board
+import neopixel
 
-image = "images/fire.jpg"
+image_src = "images/fire.jpg"
 leds_pin = board.D18
 leds_num = 25
 max_brightness = 1.0
 speed = 100 # y-pixels per second
 intencivity =  1 # new flame per second
-fps = 25
+fps = 1
+# fps = 25
 
 leds = neopixel.NeoPixel(leds_pin, leds_num, brightness = max_brightness, auto_write=False, pixel_order = neopixel.GRB)
 
-animation = Animation(image, leds_num, max_brightness, speed, intencivity)
 
-frame_ms = 1000 // fps
+animation = Animation(image_src, leds_num, max_brightness, speed, intencivity)
+
+frame_ms = 1 // fps
 
 while True:
     animation.process()
-    for i, led in animation.active_row:
+    i = 0
+    for led in animation.active_row:
         leds[i] = led
+        i =+1
     leds.show()
     time.sleep(frame_ms)
-
 
 
