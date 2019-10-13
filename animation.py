0f8109c0fbd6abc6abd87847889864362e12c69b
lavron <lavron@lavron.info>
2019-10-13 18:43:04 +0300

    fixws

--- animation.py
+++ animation.py
@@ -16,75 +16,71 @@ class Animation:
     def __init__(self, image_src, leds_num, brightness, speed, intencivity):
 
         self.brightness = brightness
-        # self.image_src = image_src
-        self.speed = speed / 1000 # rows per ms
+
+        self.row_ms = 1000 / speed
         
-        self.intencivity = intencivity * 1000 # new flame per ms
-        
-        self.image = Image.open(image_src)   
+        self.image = Image.open(image_src).convert('RGB')
         
         width, height = self.image.size
         proportion = height / width
 
         self.image = self.image.resize((leds_num, int(leds_num * proportion)))
-        self.width, self.height = self.image.size
+        self.surface  = Image.new('RGB', self.image.size, (0,0,0))
+
+        self.width, self.height = self.surface.size
 
         self.active_row = [(0,0,0)] * self.width
-        
         self.ts =int(round(time.time() * 1000))
-
         self.frame_count = 0
 
-    
+        self.intencivity_max = int(intencivity * 1.5 * 1000)
+        self.intencivity_min = int(intencivity * 0.5 * 1000)
+
+        self.flame_next_in_ms = rand(self.intencivity_min, self.intencivity_max)
+        self.flamed_ms = 0
 
     def add_image(self):
-        min_width = self.image.width // 5
-        min_heigh = self.image.height // 5
 
-        width = rand(min_width, self.image.width)
-        heigh = rand(min_heigh, self.image.height)
+        self.flame_next_in_ms = rand(self.intencivity_min, self.intencivity_max)
+        self.flamed_ms = 0
+
+        min_width = self.surface.width // 5
+        min_heigh = self.surface.height // 5
+
+        width = rand(min_width, self.surface.width)
+        heigh = rand(min_heigh, self.surface.height)
         size = (width, heigh)
         opacity = rand(1, int(self.brightness * 10)) / 10
 
-        new_image =  self.image.copy()
-        new_image = new_image.resize(size)
+        new_image =  self.image.copy().resize(size)
 
         xy = (rand(0, self.image.width - width), 0)
-        self.image.paste(new_image, xy, new_image.convert('RGB'))
 
-        print("new image created")
-
-        new_image.show()
+        self.surface.paste(new_image, xy)
     
         del new_image
+
+        print("new image added")
 
     def process(self):
         self.frame_count +=1
         ts = time.time() * 1000
-        elapsed = int(ts - self.ts)
-        offset = elapsed  % (self.speed * 1000) / (self.speed * 1000)
-        move = self.height * offset
+        elapsed_ms = int(ts - self.ts)
+        move = round(elapsed_ms / self.row_ms)
 
-        w, h = self.image.size
+        self.flamed_ms += elapsed_ms
 
-        print("offset:", offset)
-        print("elapsed:", elapsed)
-        print("move:", move)
-        print()
+        w, h = self.surface.size
 
         for i in range(self.width):
-            self.active_row[i] =  self.image.getpixel((i, move))
+            self.active_row[i] = self.surface.getpixel((i, move))
             
         bg = Image.new('RGB', (w, h), (0,0,0))
+        self.surface = self.surface.crop((0, move, w, h))
+        bg.paste(self.surface, (0,0) )
+        self.surface = bg
 
-        
-        self.image.crop((0, 30, w, h))
-        
-        bg.paste(self.image, (0,0) )
-
-        self.image = bg
-        
-        if one_chance_from(self.intencivity):
+        if self.flamed_ms > self.flame_next_in_ms:
             self.add_image()
             
         del bg 
--- main.py
+++ main.py
@@ -7,30 +7,29 @@ import board
 import neopixel
 
 
-image_src = "images/fire.jpg"
+image_src = "images/fire2.jpg"
 leds_pin = board.D18
 leds_num = 25
 max_brightness = 1.0
-speed = 100 # y-pixels per second
-intencivity =  1 # new flame per second
-fps = 1
-# fps = 25
+speed = 10 # rows per second 
+intencivity = 1 # new flame per second
+# fps = 2
+fps = 25
 
 leds = neopixel.NeoPixel(leds_pin, leds_num, brightness = max_brightness, auto_write=False, pixel_order = neopixel.GRB)
 
 
 animation = Animation(image_src, leds_num, max_brightness, speed, intencivity)
 
-sleep_s = 1 // fps
+sleep_s = 1 / fps
 
 while True:
+    # print("frame:", animation.active_row)
     animation.process()
     i = 0
-    print(animation.active_row)
     for led in animation.active_row:
         leds[i] = led
         i +=1
-    # print(leds)
     leds.show()
     time.sleep(sleep_s)
 
