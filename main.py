2d099be93e27ba30ab7b20360b010a4795857cef
Viktor Lavron <lavron.mail@lavron.info>
2019-10-22 10:59:08 +0300

    v0.2.1

--- main.py
+++ main.py
@@ -1,36 +1,35 @@
-#!/usr/bin/env python
-
+import neopixel
 from img2neopixel import Animation
 import time
-import board
-import neopixel
 
+PATH = "/home/pi/python/humax/"
 
-image_src = "images/fire2.jpg"
-# image_src = "images/amoled-4.jpg"
-# image_src = "images/Fire-Wallpaper-HD.jpg"
-leds_pin = board.D18
+image_src = PATH + "images/fire.jpg"
+leds_pin = 18
 
 leds_num = 25
-max_brightness = 0.5
-speed = 25 # rows per second 
-intencivity = 0.5 # new flame per second
+max_brightness = 1
+speed = 25  # rows per second
+intencivity = 1  # new flame per second
 # fps = 2
 fps = 25
 sleep_s = 1 / fps
 
-
-leds = neopixel.NeoPixel(leds_pin, leds_num, brightness = max_brightness, auto_write=False, pixel_order = neopixel.GRB)
+strip = neopixel.NeoPixel(leds_pin,
+                          leds_num,
+                          brightness=max_brightness,
+                          auto_write=False,
+                          pixel_order=neopixel.GRB)
 animation = Animation(image_src, leds_num, max_brightness, speed, intencivity)
 
 
 while True:
+    time.sleep(sleep_s)
+
     animation.process()
     i = 0
     for led in animation.active_row:
-        leds[i] = led
-        i +=1
-    leds.show()
-    time.sleep(sleep_s)
-
+        strip[i] = led
+        i += 1
+    strip.show()
 
