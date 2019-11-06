f38a0665d78b56786097d542b3dbf61769ca43bc
lavron <lavron@lavron.info>
2019-11-06 20:00:09 +0000

    many fixes

--- img2neopixel.py
+++ img2neopixel.py
@@ -6,29 +6,45 @@ import datetime
 from PIL import Image
 import time
 import sys
+import os
+
 
 color_scheme = 'RGB'
+
+abspath = os.path.abspath(__file__)
+root_dir = os.path.dirname(abspath) + "/images/"
 
 OFF = 0
 FIRE = 1
 FADEOUT = 2
 
 class SingleAnimation:
-    def __init__(self, strip, image_src, duration_s, *fps):
+    def __init__(self, strip, images_src, duration_s, *fps):
         fps = fps or 25
+        # fps = fps or 1
         self.frame = 0
         self.brightness = 127 # 0-255g
         self.width = strip['num']
         self.height = duration_s * fps
 
-        self.image_src = image_src
+        self.images_src = images_src
+        self.images = []
 
-        try:
-            self.image = Image.open(image_src).convert(color_scheme)
-        except Exception as e:
-            print ("Exception:", str(e))
+        for image_src in images_src:
+            try:
+                image = Image.open(root_dir + image_src).convert(color_scheme)
+                image = image.resize((self.width, self.height))
+                # image.save(root_dir + "resized_" + image_src)
+                self.images.append(image) 
+
+            except OSError as e:
+                print ("Error:", str(e))
+
+        self.images_count = len(self.images)
+
+        if self.images_count == 0:
+            print("no images")
             sys.exit(1)
-        self.image = self.image.resize((self.width, self.height))
 
         self.strip = neopixel.NeoPixel(strip['pin'],
                           strip['num'],
@@ -36,21 +52,27 @@ class SingleAnimation:
                           auto_write=False,
                           pixel_order=neopixel.GRB)
 
-        print("strip:", sys.getsizeof(self.strip))
-        print("image", sys.getsizeof(self.image))
+
+        self.active_image_id = 0
+        self.active_image = self.images[self.active_image_id]
 
         self.active = True
 
+        print("boot finished")
+
     def move_to_next_frame(self):
 
-        if self.frame == self.image.size[1]:
+        if self.frame == self.active_image.size[1]:
+            self.active_image_id +=1
+            if self.active_image_id == self.images_count:
+                self.active_image_id = 0
             self.frame = 0
-            print("restart image")
+            self.active_image = self.images[self.active_image_id]
+            print("start image", self.images_src[self.active_image_id])
 
         for i in range(self.width):
-            self.strip[i] = self.image.getpixel((i,self.frame))
+            self.strip[i] = self.active_image.getpixel((i,self.frame))
 
         self.strip.show()
 
         self.frame +=1
-        
--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 from img2neopixel import SingleAnimation
 import time
-import os
 import board
 
-abspath = os.path.abspath(__file__)
-dname = os.path.dirname(abspath)
 
+
+images_src = (
+    # '00.jpg',
+    # '01.jpg',
+    '02.jpg',
+    # '03.jpg',
+    # '04.jpg',
+)
 
 strip = {
     'pin' : board.D18,
     'num' : 25
 }
-image_src = "/images/fire.jpg"
-duration_s = 10
+# image_src = "/images/fire.jpg"
+duration_s = 40
 
-image_src = dname + image_src
-
-animation = SingleAnimation(strip, image_src, duration_s)
+animation = SingleAnimation(strip, images_src, duration_s)
 
 while animation.active:
     time.sleep(0.04) # == 25fps
