12618186467bb35fc6148ae3601d41582176d6c4
lavron <lavron@lavron.info>
2019-11-07 18:55:15 +0000

    new faetures

--- img2neopixel.py
+++ img2neopixel.py
@@ -19,24 +19,25 @@ FIRE = 1
 FADEOUT = 2
 
 class SingleAnimation:
+
+    active_image_id = 0
+    frame = 0
+    active = True
+    images = []
+    
     def __init__(self, strip, images_src, duration_s, *fps):
         fps = fps or 25
         # fps = fps or 1
-        self.frame = 0
-        self.brightness = 127 # 0-255g
         self.width = strip['num']
         self.height = duration_s * fps
 
         self.images_src = images_src
-        self.images = []
 
         for image_src in images_src:
             try:
                 image = Image.open(root_dir + image_src).convert(color_scheme)
                 image = image.resize((self.width, self.height))
-                # image.save(root_dir + "resized_" + image_src)
                 self.images.append(image) 
-
             except OSError as e:
                 print ("Error:", str(e))
 
@@ -48,31 +49,67 @@ class SingleAnimation:
 
         self.strip = neopixel.NeoPixel(strip['pin'],
                           strip['num'],
-                          brightness=self.brightness,
-                          auto_write=False,
-                          pixel_order=neopixel.GRB)
+                          brightness=0.5,
+                          auto_write=False)
 
-
-        self.active_image_id = 0
         self.active_image = self.images[self.active_image_id]
 
+        print("img2neopixel boot finished")
+
+    def on(self):
         self.active = True
 
-        print("boot finished")
+    def off(self):
+        self.active = False
+    
+    def brightness_set(self, brightness):
+        self.strip.brightness = min (1, max(0, brightness))
 
+    def brightness_plus(self):
+        if self.strip.brightness < 1:
+           self.strip.brightness =+ 0.1
+
+    def brightness_minus(self):
+        if self.strip.brightness > 0:
+            self.strip.brightness =- 0.1
+
+    def faster():
+        pass
+    def slower():
+        pass
+
+    def set_image(self, image_id):
+        try:
+            self.active_image_id = self.images[image_id]
+        except IndexError:
+            print('sorry, no image', image_id)
+
+    def prev_image(self):
+        self.active_image_id -=1
+        if self.active_image_id == 0:
+            self.active_image_id = self.images_count
+        self.frame = 0
+        self.active_image = self.images[self.active_image_id]
+
+    def next_image(self):
+        self.active_image_id +=1
+        if self.active_image_id == self.images_count:
+            self.active_image_id = 0
+        self.frame = 0
+        self.active_image = self.images[self.active_image_id]
+        
+        print("start image", self.images_src[self.active_image_id])
+    
     def move_to_next_frame(self):
-
         if self.frame == self.active_image.size[1]:
-            self.active_image_id +=1
-            if self.active_image_id == self.images_count:
-                self.active_image_id = 0
-            self.frame = 0
-            self.active_image = self.images[self.active_image_id]
-            print("start image", self.images_src[self.active_image_id])
+            self.next_image()
 
+        self.show_frame()
+        self.frame +=1
+
+    def show_frame(self):
         for i in range(self.width):
             self.strip[i] = self.active_image.getpixel((i,self.frame))
 
         self.strip.show()
 
-        self.frame +=1
--- main.py
+++ main.py
@@ -5,23 +5,26 @@ import board
 
 
 images_src = (
-    # '00.jpg',
-    # '01.jpg',
+    '00.jpg',
+    '01.jpg',
     '02.jpg',
-    # '03.jpg',
-    # '04.jpg',
+    '03.jpg',
+    '04.jpg',
 )
 
 strip = {
     'pin' : board.D18,
     'num' : 25
 }
-# image_src = "/images/fire.jpg"
+
 duration_s = 40
 
 animation = SingleAnimation(strip, images_src, duration_s)
 
-while animation.active:
-    time.sleep(0.04) # == 25fps
-    # time.sleep(1) # == 25fps
-    animation.move_to_next_frame()
+def process_animation():
+    while animation.active:
+        time.sleep(0.04) # == 25fps
+        # time.sleep(1) # == 25fps
+        animation.move_to_next_frame()
+
+process_animation()
